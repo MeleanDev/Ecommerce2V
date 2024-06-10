21accd8a50aac93294d5f028c7118c8e9f790352
Luis Acosta <gitmeleandev.phantom644@passinbox.com>
2024-06-10 17:39:49 -0400

    depuracion

--- app/Http/Controllers/admin/EmpresaController.php
+++ app/Http/Controllers/admin/EmpresaController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\admin\EmpresaRequest;
 use App\Service\admin\EmpresaClass;
 use Illuminate\Http\Request;
 
